e7d8f65999193a7b2519c5c8486b14b14bec5f40
Matthias <werner.matthias@web.de>
2024-12-08 17:33:46 +0100

    basic speedometer

--- src/main.cpp
+++ src/main.cpp
@@ -30,9 +30,10 @@
 TFT_eSPI tft = TFT_eSPI(); // Create object "tft"
 
 TFT_eSprite needle = TFT_eSprite(&tft); // Create Sprite object "needle" with pointer to "tft" object
-                                     // the pointer is used by pushSprite() to push it onto the TFT
+                                        // the pointer is used by pushSprite() to push it onto the TFT
 
-TFT_eSprite background = TFT_eSprite(&tft); 
+TFT_eSprite background = TFT_eSprite(&tft);
+TFT_eSprite img = TFT_eSprite(&tft); // Create Sprite object "needle" with pointer to "tft" object
 
 // Size of sprite
 #define IWIDTH 240
@@ -43,69 +44,60 @@ int i = 0;
 // #########################################################################
 // Draw a number in a rounded rectangle with some transparent pixels
 // #########################################################################
-// void numberBox(int x, int y, float num)
-// {
-
-// // Size of sprite
-// #define IWIDTH 80
-// #define IHEIGHT 35
-
-//   // Create a 8-bit sprite 80 pixels wide, 35 high (2800 bytes of RAM needed)
-//   needle.setColorDepth(8);
-//   needle.createSprite(IWIDTH, IHEIGHT);
-
-//   // Fill it with black (this will be the transparent colour this time)
-//   needle.fillSprite(TFT_BLACK);
-
-//   // Draw a background for the numbers
-//   needle.fillRoundRect(0, 0, 80, 35, 15, TFT_RED);
-//   needle.drawRoundRect(0, 0, 80, 35, 15, TFT_WHITE);
-
-//   // Set the font parameters
-//   needle.setTextSize(1);          // Font size scaling is x1
-//   needle.setTextColor(TFT_WHITE); // White text, no background colour
-
-//   // Set text coordinate datum to middle right
-//   needle.setTextDatum(MR_DATUM);
-
-//   // Draw the number to 3 decimal places at 70,20 in font 4
-//   needle.drawFloat(num, 3, 70, 20, 4);
-
-//   // Push sprite to TFT screen CGRAM at coordinate x,y (top left corner)
-//   // All black pixels will not be drawn hence will show as "transparent"
-//   needle.pushSprite(100, 100, TFT_BLACK);
-
-//   // Delete sprite to free up the RAM
-//   needle.deleteSprite();
-// }
-
-void showNeedle(int angle)
+void numberBox(int x, int y, float num)
 {
 
-
+// Size of sprite
+#define IWIDTHx 80
+#define IHEIGHTx 35
 
   // Create a 8-bit sprite 80 pixels wide, 35 high (2800 bytes of RAM needed)
-  needle.setColorDepth(8);
-  needle.createSprite(IWIDTH, IHEIGHT);
+  img.setColorDepth(8);
+  img.createSprite(IWIDTHx, IHEIGHTx);
 
   // Fill it with black (this will be the transparent colour this time)
-  needle.fillSprite(0x041f);
+  img.fillSprite(TFT_BLACK);
 
-  // push image needle to sprite
-  needle.setSwapBytes(true);
-  needle.pushImage(0, 0, 240, 240, _needle);
+  // Draw a background for the numbers
+  img.fillRoundRect(0, 0, 80, 35, 15, TFT_RED);
+  img.drawRoundRect(0, 0, 80, 35, 15, TFT_WHITE);
 
-  // rotate the needle
-  needle.setPivot(120, 120);
-  //needle.setRotation(angle);
-   
+  // Set the font parameters
+  img.setTextSize(1);          // Font size scaling is x1
+  img.setTextColor(TFT_WHITE); // White text, no background colour
+
+  // Set text coordinate datum to middle right
+  img.setTextDatum(MR_DATUM);
+
+  // Draw the number to 3 decimal places at 70,20 in font 4
+  img.drawFloat(num, 0, 70, 20, 4);
 
   // Push sprite to TFT screen CGRAM at coordinate x,y (top left corner)
   // All black pixels will not be drawn hence will show as "transparent"
-  //needle.pushSprite(0, 0, 0x041f);
-  needle.pushSprite(0, 0, 0x041f);
+  img.pushSprite(x, y, TFT_BLACK);
+
   // Delete sprite to free up the RAM
-  needle.deleteSprite();
+  img.deleteSprite();
+}
+
+/*! ******************************************************************
+  @brief    Show a needle on the screen at a given engine speed
+  @details  This function will show a needle on the screen at a given
+          engine speed. The needle will be rotated to the correct
+          angle to represent the engine speed.
+
+  @param    speed  The engine speed in RPM
+*/
+void showNeedle(int speed)
+{
+  int angle = map(speed, 0, 4000, 226, 386);
+  // Limit the angle to 0-359
+  if (angle > 359)
+  {
+    angle = angle - 360;
+  }
+  // Draw the needle at the given angle
+  needle.pushRotated(&background, angle, 0x041f);
 }
 
 void setup(void)
@@ -127,37 +119,34 @@ void setup(void)
   background.createSprite(IWIDTH, IHEIGHT);
   background.pushImage(0, 0, 240, 240, _scale);
 
- 
   needle.createSprite(20, 100);
   needle.setColorDepth(16);
   needle.setSwapBytes(true);
   needle.pushImage(0, 0, 20, 100, _needle);
   needle.setPivot(10, 100);
   // //needle.fillSprite(0x041f);  // Fill sprite with transparent colour
-  //needle.pushRotated(&background,90);
-
-
-  
+  // needle.pushRotated(&background,90);
 }
 
 void loop()
 {
 
-  //needle.fillSprite(TFT_RED);
-  //needle.fillSprite(0x041f);
+  // needle.fillSprite(TFT_RED);
+  // needle.fillSprite(0x041f);
+  uint32_t dt = millis();
   background.pushImage(0, 0, 240, 240, _scale);
-  needle.pushRotated(&background,i,0x041f);
+  showNeedle(i);
   background.pushSprite(0, 0);
 
-  i++;
-  if (i > 360)
+  // Show time in milliseconds to draw and then push 1 sprite to TFT screen
+  // numberBox( 100, 100, (millis()-dt));
+  numberBox(100, 100, i);
+  delay(1000);
+
+  i = i + 250;
+  if (i > 4000)
   {
     i = 0;
   }
-  delay(10);
-
-  // Show time in milliseconds to draw and then push 1 sprite to TFT screen
-  // numberBox( 10, 10, (millis()-dt)/500.0 );
-
-  //delay(500);
+  // delay(10);
 }
