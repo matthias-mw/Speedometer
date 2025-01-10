4a8809a702f94e6133553c0b5459962174667fd4
Matthias <werner.matthias@web.de>
2025-01-10 21:29:24 +0100

    Update main.cpp with project documentation and remove outdated comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,20 @@
-/*
-  Sketch to show creation of a sprite with a transparent
-  background, then plot it on the TFT.
+/*!
+ * \file main.cpp
+ * \brief Main File fot the Speedometer Project
+ * 
+ * This file contains the main loop for the Speedometer project.
+ * 
+ * \author Matthias Werner
+ * \date   January 2025
+ * \version 0.1
+ *  * 
+ */
 
-  Example for library:
-  https://github.com/Bodmer/TFT_eSPI
 
-  A Sprite is notionally an invisible graphics screen that is
-  kept in the processors RAM. Graphics can be drawn into the
-  Sprite just as it can be drawn directly to the screen. Once
-  the Sprite is completed it can be plotted onto the screen in
-  any position. If there is sufficient RAM then the Sprite can
-  be the same size as the screen and used as a frame buffer.
-
-  A 16-bit Sprite occupies (2 * width * height) bytes in RAM.
-
-  On a ESP8266 Sprite sizes up to 126 x 160 can be accommodated,
-  this size requires 40kBytes of RAM for a 16-bit colour depth.
-
-  When 8-bit colour depth sprites are created they occupy
-  (width * height) bytes in RAM, so larger sprites can be
-  created, or the RAM required is halved.
-*/
 
 #include <hardwareDef.h>
 #include <Arduino.h>
 #include "displayCtl.h"
-
-
 
 int i = 10;
 
@@ -48,8 +36,6 @@ void setup(void)
 void loop()
 {
 
-  // needle.fillSprite(TFT_RED);
-  // needle.fillSprite(0x041f);
   uint32_t dt = millis();
 
   if (i > 100)
